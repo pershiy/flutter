720e31e1c60b04817e2ec338a95b5e75c04c8020
pershiy <ero1217@gmail.com>
2019-08-16 18:15:30 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
+class MyBody extends StatelessWidget {
+  Widget build(BuildContext context){
+    return new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text("Continue"),
+            new FlatButton(
+              onPressed: (){}, 
+              child: new Text("Button"),
+              )
+          ],  
+        ),
+      );
+  }
+}
+   
+
+
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
     home: new Scaffold(
       appBar: new AppBar(title: new Text("Welcom"),),
+      body: new MyBody()
       
     ),
     ));
