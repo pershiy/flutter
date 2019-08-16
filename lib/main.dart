6e631c64b7f0364385fa80a7b9b59e7137c3e592
pershiy <ero1217@gmail.com>
2019-08-16 17:03:53 +0300

    new

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new Scaffold(
+      appBar: new AppBar(title: new Text(""),),
+      
+    ),
+    ));
+}
